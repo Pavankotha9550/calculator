fb3e169dc625e00d381e788b7d15396c08434d32
pavan_garu <pavankotha9550@outlook.com>
2025-03-26 10:45:55 +0000

    developed .js for our calculator

---
+++ cal.js
@@ -0,0 +1,36 @@
+#developed a basic calculator#
+document.addEventListener("keydown", function(event) {
+	    let key = event.key;
+
+	    if (!isNaN(key) || "+-*/.".includes(key)) {
+		            appendValue(key);
+		        } else if (key === "Enter") {
+				        calculateResult();
+				    } else if (key === "Backspace") {
+					            backspace();
+					        } else if (key === "Escape") {
+							        clearDisplay();
+							    }
+});
+
+function appendValue(value) {
+	    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+	    document.getElementById("display").value = "";
+}
+
+function calculateResult() {
+	    try {
+		            document.getElementById("display").value = eval(document.getElementById("display").value);
+		        } catch {
+				        alert("Invalid Expression");
+				    }
+}
+
+function backspace() {
+	    let display = document.getElementById("display");
+	    display.value = display.value.slice(0, -1);
+}
+
